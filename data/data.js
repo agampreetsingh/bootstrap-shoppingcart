a2a42e2a647e5e52b736b960af0e9ce596f3476c
agampreetsingh <agampreet_singh@yahoo.com>
2020-04-17 22:23:32 +0530

    Adding dynamic Product List

---
+++ data/data.js
@@ -0,0 +1,79 @@
+var productList = [
+    {   
+        id:"Product_1",
+        name: "Product1",
+        price: 1000,
+        Description: "Sample Product Description of the product...",
+        productImage: "images/dummyproduct.jpg"
+    },
+    {   
+        id:"Product_2",
+        name: "Product2",
+        price: 2000,
+        Description: "Sample Product Description of the product...",
+        productImage: "images/dummyproduct.jpg"
+    },
+    {   
+        id:"Product_3",
+        name: "Product3",
+        price: 10000,
+        Description: "Sample Product Description of the product...",
+        productImage: "images/dummyproduct.jpg"
+    },
+    {   
+        id:"Product_4",
+        name: "Product4",
+        price: 1500,
+        Description: "Sample Product Description of the product...",
+        productImage: "images/dummyproduct.jpg"
+    },
+    {   
+        id:"Product_5",
+        name: "Product5",
+        price: 11000,
+        Description: "Sample Product Description of the product...",
+        productImage: "images/dummyproduct.jpg"
+    },
+    {   
+        id:"Product_6",
+        name: "Product6",
+        price: 4000,
+        Description: "Sample Product Description of the product...",
+        productImage: "images/dummyproduct.jpg"
+    },
+    {   
+        id:"Product_7",
+        name: "Product7",
+        price: 100000,
+        Description: "Sample Product Description of the product...",
+        productImage: "images/dummyproduct.jpg"
+    },
+    {   
+        id:"Product_8",
+        name: "Product8",
+        price: 7000,
+        Description: "Sample Product Description of the product...",
+        productImage: "images/dummyproduct.jpg"
+    },
+    {   
+        id:"Product_9",
+        name: "Product9",
+        price: 25000,
+        Description: "Sample Product Description of the product...",
+        productImage: "images/dummyproduct.jpg"
+    },
+    {   
+        id:"Product_10",
+        name: "Product10",
+        price: 1000,
+        Description: "Sample Product Description of the product...",
+        productImage: "images/dummyproduct.jpg"
+    },
+    {   
+        id:"Product_11",
+        name: "Product11",
+        price: 1050,
+        Description: "Sample Product Description of the product...",
+        productImage: "images/dummyproduct.jpg"
+    }
+];
